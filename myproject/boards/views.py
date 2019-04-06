c5a7f5c03b7597a729db2d643b8e83a95895a370
OASans <171860015@smail.nju.edu.cn>
2019-04-07 00:42:03 +0800

    better page navigation & add gravatar

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -8,6 +8,7 @@ from django.db.models import Count
 from django.views.generic import UpdateView,ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
+from django.urls import reverse
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
@@ -74,8 +75,11 @@ class PostListView(ListView):
 
 
     def get_context_data(self, **kwargs):
-        self.topic.views +=1
-        self.topic.save()
+        session_key='viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key,False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key]=True
         kwargs['topic']=self.topic
         return super().get_context_data(**kwargs)
 
@@ -120,7 +124,17 @@ def reply_topic(request,pk,topic_pk):
             post.topic=topic
             post.created_by=request.user
             post.save()
-            return redirect('topic_posts',pk=pk,topic_pk=topic_pk)
+
+            topic.last_updated=timezone.now()
+            topic.save()
+
+            topic_url=reverse('topic_posts',kwargs={'pk':pk,'topic_pk':topic_pk})
+            topic_post_url='{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+            return redirect(topic_post_url)
     else:
         form=PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
