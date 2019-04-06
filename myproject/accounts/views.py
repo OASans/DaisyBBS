13c064cc79f8a6e366cfb729ec056fdc9dcd01c3
OASans <171860015@smail.nju.edu.cn>
2019-04-06 18:59:26 +0800

    add pagination

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
 from .forms import SignUpForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
 
 # Create your views here.
 def signup(request):
@@ -14,3 +19,15 @@ def signup(request):
     else:
         form=SignUpForm()
     return render(request,'signup.html',{'form':form})
+
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('first_name','last_name','email',)
+    template_name = 'my_account.html'
+    success_url = reverse_lazy('my_account')
+
+
+    def get_object(self):
+        return self.request.user
